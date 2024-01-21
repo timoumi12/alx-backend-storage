f3fd9b219a5acd8aae4e6b7c56d1a6f36c393d92
timoumi12 <ihebtimoumi1@gmail.com>
2024-01-21 12:51:15 +0100

    function and view tasks

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- creates a function SafeDiv that returns a/b
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    RETURN (IF (b = 0, 0, a / b));
+END $$
+DELIMITER;
