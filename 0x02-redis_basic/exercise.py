0763d2026d4e4978f5c03ee14e25ba31edb87c10
timoumi12 <ihebtimoumi1@gmail.com>
2024-01-25 03:42:18 +0100

    ex1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Optional[callable] = None) -> any:
+        '''take a string key argurment'''
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key):
+        '''Retrieve a string value from a Redis data storage'''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key):
+        '''Retrieve an integer value from a Redis data storage'''
+        return self.get(key, lambda x: int(x))
